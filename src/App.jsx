fb4c18ada0f8b9c28aa83c2f83a3a269ec2ab257
Ian McGuinness <ian.mcguinness@outlook.com>
2023-11-06 21:20:42 +0000

    Changed data source

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { useState, useEffect } from 'react'
 import Result from './components/Result'
 import Summary from './components/Summary'
+import data from './data'
 import './styles/App.css'
 
 const App = () => {
-  const [data, setData] = useState([])
-  const getData = () =>
-    fetch('data.json', {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
-      }
-    })
-      .then(res => res.json())
-      .then(data => setData(data))
-
-  useEffect(() => {
-    getData()
-  }, [])
   const avg = Math.round(
     data.reduce((acc, cur) => acc + cur.score, 0) / data.length
   )
